34a77ed422194ae6f08ddd6258fe58bbff752365
Paul Bérard <paulberard@Pauls-Laptop.local>
2023-04-24 04:38:53 +0200

    added option to drop neutral class

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,32 @@ from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer
 from typing import List
 
+# import random
+# import pandas as pd
+
+# def undersample(df, target_col):
+#     # get class counts
+#     class_counts = df[target_col].value_counts()
+    
+#     # get size of minority class
+#     min_class_size = class_counts.min()
+    
+#     # create empty dataframe for undersampled data
+#     undersampled_df = pd.DataFrame(columns=df.columns)
+    
+#     # for each class
+#     for class_label in class_counts.index:
+#         # get instances of the class
+#         class_df = df[df[target_col] == class_label]
+        
+#         # randomly select instances to keep
+#         keep_indices = random.sample(range(len(class_df)), min_class_size)
+#         keep_df = class_df.iloc[keep_indices, :]
+        
+#         # add undersampled instances to the new dataframe
+#         undersampled_df = undersampled_df.append(keep_df)
+        
+#    return undersampled_df
 
 class ProcessGoEmotions:
     positive = [
@@ -27,12 +53,12 @@ class ProcessGoEmotions:
         self.label_choice = label_choice
         self.df = None
     
-    def get_datasets(self, paths: List[str], test_size: float = 0.2):
+    def get_datasets(self, paths: List[str], test_size: float = 0.2, drop_neutral: bool = True):
         """ Main method """
         self.load_df(paths)
         self.encode()
         self.define_label()
-        train_dataset, test_dataset = self.split_dataset(test_size)
+        train_dataset, test_dataset = self.split_dataset(test_size, drop_neutral)
         return train_dataset, test_dataset
 
     def load_df(self, paths: List[str]):
@@ -74,13 +100,20 @@ class ProcessGoEmotions:
         self.df = self.df[["text", "emotions", "emotion_category"]].copy()
 
     def define_label(self):
-        assert self.label_choice in self.df.columns, "Label choice must be emotions or emotion_category."
+        assert self.label_choice in self.df.columns, "Label choice must be 'emotions' or 'emotion_category'."
     
         self.df.rename({self.label_choice: "label"}, inplace=True, axis = 1)
         self.df = self.df[["text", "label"]]
 
-    def split_dataset(self, test_size: float = 0.2):
-        train, test = train_test_split(self.df, test_size=test_size, stratify=self.df.label, random_state=42,)
+    def split_dataset(self, test_size: float = 0.2, drop_neutral: bool = True):
+        _, mapping, _ = self.get_mapping()
+        if drop_neutral:
+            print("Removing 'neutral'")
+            self.df = self.df[self.df.label != mapping['neutral']]
+        
+        # self.df = undersample(self.df, 'label')
+        
+        train, test = train_test_split(self.df, test_size=test_size, stratify=self.df.label, random_state=42)
         train.reset_index(drop=True, inplace=True)
         test.reset_index(drop=True, inplace=True)
         return train, test
